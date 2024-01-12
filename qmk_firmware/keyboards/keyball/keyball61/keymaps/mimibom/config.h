9b5cf4ab32cda48e59581781dc2e5e792f05c6bd
mimibom <miyatamasato64@gmail.com>
2024-01-12 21:55:24 +0900

    白LEDしか点かん

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/mimibom/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/mimibom/config.h
@@ -31,6 +31,48 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#    define RGBLIGHT_EFFECT_RGB_TEST
 //#    define RGBLIGHT_EFFECT_ALTERNATING
 //#    define RGBLIGHT_EFFECT_TWINKLE
+#   define RGBLIGHT_LAYERS
+#   define RGBLIGHT_DEFAULT_SAT 64
+#   define RGBLIGHT_DEFAULT_HUE 169
 #endif
 
+#define LED_LAYOUT( \
+    L00, L01, L02, L03, L04, L05,              R05, R04, R03, R02, R01, R00, \
+    L10, L11, L12, L13, L14, L15,              R15, R14, R13, R12, R11, R10, \
+    L20, L21, L22, L23, L24, L25,              R25, R24, R23, R22, R21, R20, \
+    L30, L31, L32, L33, L34, L35, L36,    R36, R35, R34, R33, R32, R31, R30, \
+    L40, L41, L42, L43,                                            R41, R40, \
+    BL00, BL01,                                                  BR00, BR01, \
+    BL02,                                                              BR02, \
+    BL03, BL04,      BL05, BL06, BL07,    BR03, BR04, BR05,            BR06  \
+    ) \
+    { \
+    L36, L05, L15, L25, L35, \
+    L04, L14, L24, L34, L03, \
+    L13, L23, L33, L43, L02, \
+    L12, L22, L32, L42, L01, \
+    L11, L21, L31, L41, L00, \
+    L10, L20, L30, L40, \
+    BL01, BL00, BL02, BL03, BL04, BL05, BL06, BL07, \
+    BR03, BR04, BR05, BR06, BR02, BR01, BR00, \
+    R00, R10, R20, \
+    R30, R40, R01, R11, R21, \
+    R31, R41, R02, R12, R22, \
+    R32, R03, R13, R23, R33, \
+    R04, R14, R24, R34, R05, \
+    R15, R25, R35, R36 \
+    }
+
+#define RGBLIGHT_LED_MAP LED_LAYOUT(\
+     0,  1,  2,  3,  4,  5,          37, 38, 39, 40, 41, 42, \
+     6,  7,  8,  9, 10, 11,          43, 44, 45, 46, 47, 48, \
+    12, 13, 14, 15, 16, 17,          49, 50, 51, 52, 53, 54, \
+    18, 19, 20, 21, 22, 23, 24,  55, 56, 57, 58, 59, 60, 61, \
+    25, 26, 27, 28,                                  62, 63, \
+    29, 30,                                          64, 65, \
+    31,                                                  66, \
+    32, 33, 34,         35, 36,  67, 68, 69,             70  \
+)
+
 #define TAP_CODE_DELAY 5
+
--- qmk_firmware/keyboards/keyball/keyball61/keymaps/mimibom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/mimibom/keymap.c
@@ -20,16 +20,23 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
+enum layer_number {
+	_QWERTY = 0,
+	_NUMBER = 1,
+	_MOUSE = 2,
+	_CONFIG = 3,
+};
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_universal(
+  [_QWERTY] = LAYOUT_universal(
     KC_1     , KC_2     , KC_3     , KC_4     , KC_5     , KC_6     ,                                  KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  , KC_EQL   ,
     KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_LBRC  ,
     KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     ,LT(2,KC_SCLN),KC_QUOT,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_RBRC ,              KC_NUHS , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
     KC_LCTL  , KC_LALT  , KC_LGUI  , KC_MUTE  , MO(1)    ,LT(2,KC_SPC), LT(3,KC_ESC),        KC_BSPC ,LT(2,KC_ENT), KC_RGUI,  _______ ,  _______ , TG(1)    , SCRL_MO 
   ),
-  [1] = LAYOUT_universal(
+  [_NUMBER] = LAYOUT_universal(
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  KC_NO    , KC_PSLS  , KC_PAST  , KC_PMNS  , _______  , KC_GRV   ,
     _______  , _______  , KC_UP    , _______  , _______  , _______  ,                                  KC_P7    , KC_P8    , KC_P9    , KC_PPLS  , _______  , _______  ,
     _______  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , _______  ,                                  KC_P4    , KC_P5    , KC_P6    , KC_PPLS  , _______  , _______  ,
@@ -37,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  , _______  , _______  , _______  , _______  , _______  , _______ ,              KC_PDOT , KC_P0    , _______  , _______  , _______  , _______  , _______
   ),
 
-  [2] = LAYOUT_universal(
+  [_MOUSE] = LAYOUT_universal(
     KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    , KC_F6    ,                                  KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   , KC_F12   ,
     KBC_SAVE , CPI_I100 , SCRL_DVI , KC_NO    , KC_NO    , KC_NO    ,                                  KC_NO    , KC_BTN3  , KC_NO    , KC_BTN4  , KC_NO    , _______  ,
     KC_NO    , CPI_D100 , SCRL_DVD , KC_NO    , KC_NO    , KC_NO    ,                                  KC_PGUP  , KC_BTN1  , KC_NO    , KC_BTN2  , KC_NO    , _______  ,
@@ -45,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  , _______  , _______  , _______  , _______  , _______  , _______  ,             KC_DEL  , _______  , _______  , _______  , _______  , _______  , _______
   ),
 
-  [3] = LAYOUT_universal(
+  [_CONFIG] = LAYOUT_universal(
     RGB_TOG  , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                  KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,
     KBC_SAVE , RGB_HUI  , RGB_SAI  , RGB_VAI  , KC_NO    , KC_NO    ,                                  KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KBC_SAVE ,
     KC_NO    , RGB_HUD  , RGB_SAD  , RGB_VAD  , KC_NO    , KC_NO    ,                                  KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , KC_NO    , _______  ,
@@ -55,30 +62,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 // clang-format on
 
+const rgblight_segment_t PROGMEM rgb_qwerty_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 3, HSV_RED}, // Left
+    {37, 3, HSV_BLUE} // Right
+);
+
+const rgblight_segment_t PROGMEM rgb_number_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 4, HSV_GREEN}, // Left
+    {38, 4, HSV_GREEN} // Right
+);
+
+const rgblight_segment_t PROGMEM rgb_mouse_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {2, 5, HSV_PINK}, // Left
+    {39, 5, HSV_PINK} // Right
+);
+
+const rgblight_segment_t PROGMEM rgb_config_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {3, 6, HSV_CORAL}, // Left
+    {40, 6, HSV_CORAL} // Right
+);
+
+const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+	rgb_qwerty_layer,
+	rgb_number_layer,
+	rgb_mouse_layer,
+	rgb_config_layer
+);
+
+
 layer_state_t layer_state_set_user(layer_state_t state) {
-    // 手動でスクロールモードにしたいので存在しないレイヤー4に割り当てとく
-	// 本当はどうやるのかわからん
-    keyball_set_scroll_mode(get_highest_layer(state) == 4);
-    uint8_t layer = biton32(state);
-    switch (layer) {
-        case 0:
-            rgblight_sethsv(HSV_OFF);
-            break;
-        case 1:
-            rgblight_sethsv(HSV_CHARTREUSE);
-            break;
-        case 2:
-            rgblight_sethsv(HSV_ORANGE);
-            break;
-        case 3:
-            rgblight_sethsv(HSV_PURPLE);
-            break;
-    }
+	rgblight_set_layer_state(0, layer_state_cmp(state, _QWERTY));
+	rgblight_set_layer_state(1, layer_state_cmp(state, _NUMBER));
+	rgblight_set_layer_state(2, layer_state_cmp(state, _MOUSE));
+	rgblight_set_layer_state(3, layer_state_cmp(state, _CONFIG));
     return state;
 }
 
 #ifdef OLED_ENABLE
-
 #    include "lib/oledkit/oledkit.h"
 
 void oledkit_render_info_user(void) {
